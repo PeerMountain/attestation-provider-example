fc0a1624dc2259cb5c4f1c439bb49f7d0c8e9f3e
Igor Drozdov <igor@kyc3.com>
2021-07-15 13:42:42 +0200

    Initial commit

---
+++ src/main/kotlin/com/kyc3/timestampap/TimestampApApplication.kt
@@ -0,0 +1,11 @@
+package com.kyc3.timestampap
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TimestampApApplication
+
+fun main(args: Array<String>) {
+  runApplication<TimestampApApplication>(*args)
+}
