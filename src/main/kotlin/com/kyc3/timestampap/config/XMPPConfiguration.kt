3e8838b5d1849a29e365882ea882baa783151ffd
Igor Drozdov <igor@kyc3.com>
2021-07-15 18:38:54 +0200

    [PEER-76] Connect to XMPP server

---
+++ src/main/kotlin/com/kyc3/timestampap/config/XMPPConfiguration.kt
@@ -0,0 +1,34 @@
+package com.kyc3.timestampap.config
+
+import com.kyc3.timestampap.config.properties.XmppProperties
+import org.jivesoftware.smack.chat2.ChatManager
+import org.jivesoftware.smack.tcp.XMPPTCPConnection
+import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class XMPPConfiguration(
+    val xmppProperties: XmppProperties
+) {
+
+  @Bean
+  fun xmppTcpConnectionConfiguration(): XMPPTCPConnectionConfiguration =
+      XMPPTCPConnectionConfiguration.builder()
+          .setXmppDomain(xmppProperties.domain)
+          .setUsernameAndPassword(xmppProperties.userName, xmppProperties.password)
+          .setHost(xmppProperties.host)
+          .build()
+
+  @Bean
+  fun xmppTcpConnection(xmpptcpConnectionConfiguration: XMPPTCPConnectionConfiguration): XMPPTCPConnection =
+      XMPPTCPConnection(xmpptcpConnectionConfiguration)
+          .also {
+            it.connect()
+            it.login()
+          }
+
+  @Bean
+  fun chatManager(xmpptcpConnection: XMPPTCPConnection): ChatManager =
+      ChatManager.getInstanceFor(xmpptcpConnection)
+}
