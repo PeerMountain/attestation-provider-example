6f934c7dcff1b513183e6c6871c3d8269119d08e
Igor Drozdov <igor@kyc3.com>
2021-07-20 10:50:56 +0200

    [PEER-76] Transactional Hotfix

--- src/main/kotlin/com/kyc3/timestampap/repository/entity/AttestationUrlEntity.kt
+++ src/main/kotlin/com/kyc3/timestampap/repository/entity/AttestationUrlEntity.kt
@@ -1,8 +1,10 @@
 package com.kyc3.timestampap.repository.entity
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
 import java.time.LocalDateTime
 
+@Table("attestation_url")
 data class AttestationUrlEntity(
   @Id
   val id: Long,
