6a62af1e57a623d2cc19ca39c75102027b3a560e
Igor Drozdov <igor@kyc3.com>
2021-07-20 10:47:12 +0200

    [PEER-76] Transactional Hotfix

--- src/main/kotlin/com/kyc3/timestampap/service/AttestationUrlService.kt
+++ src/main/kotlin/com/kyc3/timestampap/service/AttestationUrlService.kt
@@ -6,6 +6,7 @@ import com.kyc3.timestampap.repository.UserDataRepository
 import com.kyc3.timestampap.repository.entity.AttestationUrlEntity
 import org.apache.commons.lang3.RandomStringUtils
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import reactor.core.publisher.Mono
 import java.time.LocalDateTime
 
@@ -20,6 +21,7 @@ class AttestationUrlService(
     private val TOKEN_LENGTH = 20
   }
 
+  @Transactional
   fun generate(userAddress: String): Mono<String> =
     userDataRepository.findByAddress(userAddress)
       .flatMap { user ->
