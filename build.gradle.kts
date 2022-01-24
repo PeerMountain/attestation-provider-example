c429349c669cb9e2c8fefa32b98b1b6f7cf23aff
Igor Drozdov <igor@kyc3.com>
2022-01-24 15:38:39 +0100

    [PEER-142] Library update

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
 
   implementation("org.hibernate.validator:hibernate-validator:7.0.1.Final")
 
-  implementation("com.kyc3:oracle-definitions:b220af7")
+  implementation("com.kyc3:oracle-definitions:f43c6ab")
   implementation("org.ehcache:ehcache:3.1.3")
 
   runtimeOnly("io.r2dbc:r2dbc-postgresql")
